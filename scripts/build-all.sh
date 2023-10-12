b0a277445836369bf489253c080a5a0e29af6d43
austin tabulog <austin.tabulog@gmail.com>
2023-10-12 00:29:33 -0500

    Added a project script that will run all the bits and bobs used in the full development process.

---
+++ scripts/build-all.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+#wipe any existing build folder and build it fresh
+echo "====================================================="
+echo "Resetting build directory"
+echo "====================================================="
+rm -rf ./build
+mkdir ./build && cd ./build
+
+
+#build the project according to top level CMakeLists.txt file
+echo "====================================================="
+echo "Running CMake generation"
+echo "====================================================="
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS_DEBUG="-g -fprofile-arcs -ftest-coverage" 
+
+echo "====================================================="
+echo "Building project"
+echo "====================================================="
+make -j8
+
+# run doxygen generation on code base
+echo "====================================================="
+echo "building project's documentation target"
+echo "====================================================="
+cd .. #return back to main project directory
+doxygen
+
+# run all tests with code coverage (gcov) thru ctest
+echo "====================================================="
+echo "Starting unit tests with code coverage"
+echo "====================================================="
+cd ./build #cd into build folder
+ctest -T Test -T Coverage
+
+#build lcov report from test run
+echo "====================================================="
+echo "Generating in-depth coverage report"
+echo "====================================================="
+lcov -c --directory . --output-file lcov.info
+genhtml lcov.info --output-directory coverage
+cd .. #return back to main project directory
